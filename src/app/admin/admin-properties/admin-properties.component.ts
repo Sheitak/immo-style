98f98ea5c9a656ec98502b5e794aa667e089aefa
Quentin <goupilloux.qm@orange.fr>
2020-03-25 11:18:59 +0100

    Dashboard with add form properties

---
+++ src/app/admin/admin-properties/admin-properties.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm, FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-admin-properties',
+  templateUrl: './admin-properties.component.html',
+  styleUrls: ['./admin-properties.component.css']
+})
+export class AdminPropertiesComponent implements OnInit {
+
+  propertiesForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.initPropertiesForm();
+  }
+
+  initPropertiesForm() {
+    this.propertiesForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      category: ['', Validators.required],
+      surface: ['', Validators.required],
+      rooms: ['', Validators.required],
+      description: '',
+      price: ['', Validators.required]
+    });
+  }
+
+  onSubmitPropertiesForm() {
+    // récupérer valeur indépendante exemple
+    // const title = form.value['title'];
+    console.log(this.propertiesForm.value);
+  }
+
+}
